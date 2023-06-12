fd9133a1d56478633333781eb95c22fc3c04cc21
Future Sync <futuresync@proton.me>
2023-06-12 11:22:44 -0300

    fix errors

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ var scrollTo = function(identifier, speed) {
 //    }
 //});
 
-function openMobileMenu(){ $(".mobile-menu-content").fadeIn(); }
-function section1(){ scrollTo("#section1"); }
-function section2(){ scrollTo("#section2"); }
-function section3(){ scrollTo("#section3"); }
-function section4(){ scrollTo("#section4"); }
+function openMobile(){ $(".mobile").fadeIn(); }
+function section1(){ scrollTo("#section1"); $(".mobile").fadeOut(); }
+function section2(){ scrollTo("#section2"); $(".mobile").fadeOut(); }
+function section3(){ scrollTo("#section3"); $(".mobile").fadeOut(); }
+function section4(){ scrollTo("#section4"); $(".mobile").fadeOut(); }
